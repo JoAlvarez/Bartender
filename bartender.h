bcb98c36d12c777ab76ff5c2eeaad3e1bbeb29d3
liljjbig <john.lo.alvarez@gmail.com>
2013-03-03 14:36:14 -0800

    started baisc outline for bartender.h

--- bartender.h
+++ bartender.h
@@ -1,1 +1,17 @@
 //header file for bartender class
+#ifndef __BARTENDER_H__
+#define __BARTENDER_H__
+
+#include <string>
+#include <vector>
+
+class Bartender
+{
+ private:
+  vector< vector<string> > drinks
+public:
+  Bartender(); 
+  void init();
+};
+
+#endif
